fe5612713372274677f642cd58673a2abb5dbf84
Levi Zitting <levizitting5663@gmail.com>
2017-07-31 16:18:34 -0500

    adding more

---
+++ 13 JS Modules/es6modules/app.js
@@ -0,0 +1,7 @@
+import {apiKey as key, url, sayHi} from './src/config'
+
+const ages = [1, 4, 5, 6, 4, 28];
+
+sayHi(url);
+
+console.log(apiKey, url);
---
+++ 13 JS Modules/es6modules/src/config.js
@@ -0,0 +1,12 @@
+export const apiKey = 'abc123';
+
+export const url = 'http://google.com';
+
+export function sayHi(name) {
+	console.log(`Hey there ${name}`)
+}
+
+const age = 100;
+const dog = 'Bark!';
+
+export {age, dog}
